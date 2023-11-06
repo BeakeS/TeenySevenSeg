c098a4e45c85409b4144b5090acc0b660e5d5658
BeakeS <michaelpbeakes@gmail.com>
2023-11-06 16:00:59 -0500

    Add files via upload

--- src/TeenySevenSeg.h
+++ src/TeenySevenSeg.h
@@ -28,8 +28,12 @@ class TeenySevenSeg {
 
     TeenySevenSeg(T& displayObj) : _displayObj(displayObj) {};
 
-    void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1, uint16_t color) {
-      _displayObj.drawLine(x0, y0, x1, y1, color);
+    void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color) {
+      _displayObj.drawFastHLine(x, y, w, color);
+    }
+
+    void drawFastVLine(int16_t x, int16_t y, int16_t w, uint16_t color) {
+      _displayObj.drawFastVLine(x, y, w, color);
     }
 
     void drawSSDigit(int16_t value, int16_t x, int16_t y, int16_t l, int16_t w, uint16_t color) {
@@ -72,6 +76,7 @@ class TeenySevenSeg {
           break;
       }
     }
+
     void drawSSTwoDigit(int16_t value, int16_t x, int16_t y, int16_t l, int16_t w, int16_t s, uint16_t color) {
       int16_t _value = min(value, 99);
       int digitOffset = 0;
@@ -83,9 +88,11 @@ class TeenySevenSeg {
       digitOffset+=l+s;
       drawSSDigit((_value%10), x+digitOffset, y, l, w, color);
     }
+
     void drawSSClock(uint32_t time, int16_t x, int16_t y, int16_t l, int16_t w, uint16_t color) {
       drawSSClock(time, x, y, l, w, w, w, w, color);
     }
+
     void drawSSClock(uint32_t time, int16_t x, int16_t y, int16_t l, int16_t w, int16_t s, int16_t cw, int16_t cs, uint16_t color) {
       uint32_t _time = min(time, (uint32_t) 5999);
       int mins = _time / 60;
@@ -132,33 +139,45 @@ class TeenySevenSeg {
     uint8_t _digitSegV_9[7]     = {1, 1, 1, 0, 0, 1, 1};
     uint8_t _digitSegV_error[7] = {1, 0, 0, 1, 1, 1, 1};
 
+    // Segment Map
+    //
+    //     A
+    //   F   B
+    //     G
+    //   E   C
+    //     D
+    //
+
     void drawSSSegStripe(segName_t segName, int16_t n, int16_t x, int16_t y, int16_t l, int16_t w, uint16_t color) {
       struct SegInfo { segDir_t segDir; int16_t xOffset, yOffset; } segInfo;
       // Get raw segment x0,y0
       switch(segName) {
         case SegA : segInfo={SegH, 0,     n           }; break;
-        case SegB : segInfo={SegV, l-1-n, 0           }; break;
-        case SegC : segInfo={SegV, l-1-n, l-w         }; break;
-        case SegD : segInfo={SegH, 0,     (2*l)-w-1-n }; break;
-        case SegE : segInfo={SegV, n,     l-w         }; break;
+        case SegB : segInfo={SegV, l-n-1, 0           }; break;
+     // case SegC : segInfo={SegV, l-n-1, l-w         }; break;
+        case SegC : segInfo={SegV, l-n-1, l-w+(w<3 ? 0 : (w%2==0 ? 0 : 1))       }; break;
+        case SegD : segInfo={SegH, 0,     (2*l)-w-n-1 }; break;
+     // case SegE : segInfo={SegV, n,     l-w         }; break;
+        case SegE : segInfo={SegV, n,     l-w+(w<3 ? 0 : (w%2==0 ? 0 : 1))       }; break;
         case SegF : segInfo={SegV, n,     0           }; break;
-        case SegG : segInfo={SegH, 0,     l-1-n       }; break;
+        case SegG : segInfo={SegH, 0,     l-n-1       }; break;
         default   : segInfo={SegH, 0,     n           }; break;
       }
       // Clip segment ends (this needs aesthetic tuning)
-      int16_t segClip;
-      if(segName==SegG) {
-        switch(w) {
-          case 1  : segClip=1; break;
-          case 2  : segClip=1; break;
-          case 3  : segClip=max(w-1-n, n); break;
-          default : segClip=max(w-1-n, n); break;
-        }
+      int16_t segClip = 0;
+      if(w < 2) {
+        segClip = 1;
       } else {
-        switch(w) {
-          case 1  : segClip=1; break;
-          case 2  : segClip=w-1-n; break;
-          default : segClip=max(1, w-1-n); break;
+        if(segName==SegG) {
+          if(n > (w/2)) segClip=n-(w/2);
+          if(w%2==0) {
+            if(n < ((w/2)-1)) segClip=((w/2)-1)-n;
+          } else {
+            if(n < ((w/2)-0)) segClip=((w/2)-0)-n;
+          }
+          segClip++;
+        } else {
+          if(n < (w/2)) segClip=(w/2)-n;
         }
       }
       if(segInfo.segDir==SegH) {
@@ -166,30 +185,29 @@ class TeenySevenSeg {
         //    x0<=(x+segInfo.xOffset+l-1-segClip); x0++) {
         //  drawPixel(x0, (y+segInfo.yOffset), color);
         //}
-        drawLine(x+segInfo.xOffset+segClip,     y+segInfo.yOffset,
-                 x+segInfo.xOffset+l-1-segClip, y+segInfo.yOffset,
-                 color);
+        drawFastHLine(x+segInfo.xOffset+segClip, y+segInfo.yOffset, l-(segClip*2), color);
       } else {
         //for(int16_t y0=(y+segInfo.yOffset+segClip);
         //     y0<=(y+segInfo.yOffset+l-1-segClip); y0++) {
         //  drawPixel((x+segInfo.xOffset), y0, color);
         //}
-        drawLine(x+segInfo.xOffset, y+segInfo.yOffset+segClip,
-                 x+segInfo.xOffset, y+segInfo.yOffset+l-1-segClip,
-                 color);
+        drawFastVLine(x+segInfo.xOffset, y+segInfo.yOffset+segClip, l-(segClip*2)-(w<3 ? 0 : (w%2==0 ? 0 : 1)), color);
       }
     }
+
     void drawSSSeg(segName_t segName, int16_t x, int16_t y, int16_t l, int16_t w, uint16_t color) {
       for(int16_t i=0; i<w; i++) {
         drawSSSegStripe(segName, i, x, y, l, w, color);
       }
     }
+
     void drawSSDigitSegs(const uint8_t segMask[7], int16_t x, int16_t y, int16_t l, int16_t w, uint16_t color) {
       segName_t segNameList[7] = {SegA, SegB, SegC, SegD, SegE, SegF, SegG};
       for(int i=0; i<7; i++) {
         if(segMask[i]==1) drawSSSeg(segNameList[i], x, y, l, w, color);
       }
     }
+
     void drawSSColon(int16_t x, int16_t y, int16_t l, int16_t cl, int16_t cw, uint16_t color) {
       struct SegInfo { segDir_t segDir; int16_t xOffset, yOffset; } segInfo;
       int16_t colonOffset = (l/2)-1;
@@ -199,9 +217,7 @@ class TeenySevenSeg {
           //for(int16_t k=0; k<cl; k++) {   //k=pixel count
           //  drawPixel((x+segInfo.xOffset), (y+segInfo.yOffset+colonOffset+k), color);
           //}
-          drawLine(x+segInfo.xOffset, y+segInfo.yOffset+colonOffset,
-                   x+segInfo.xOffset, y+segInfo.yOffset+colonOffset+cl-1,
-                   color);
+          drawFastVLine(x+segInfo.xOffset, y+segInfo.yOffset+colonOffset, cl, color);
         }
       }
     }
